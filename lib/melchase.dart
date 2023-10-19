90abc22598056b086f8e9d1873dea36fd0beda01
Alastier07 <calastier07@gmail.com>
2023-10-20 01:49:12 +0800

    add item bloc provider

--- lib/melchase.dart
+++ lib/melchase.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:melchase_app/screens/home_screen.dart';
 
+import 'blocs/item_bloc/item_bloc.dart';
 import 'blocs/purchase_bloc/purchase_bloc.dart';
 
 class MelchaseApp extends StatelessWidget {
@@ -12,6 +13,7 @@ class MelchaseApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => PurchaseBloc()),
+        BlocProvider(create: (context) => ItemBloc()),
       ],
       child: const MaterialApp(
         home: HomeScreen(),
