1bda53d0315e094b93457e00214c34f4a8c626b6
Alastier07 <calastier07@gmail.com>
2023-10-20 01:48:15 +0800

    separate item model

---
+++ lib/models/item_model.dart
@@ -0,0 +1,13 @@
+class Item {
+  Item({
+    required this.id,
+    required this.name,
+    required this.quantity,
+    required this.amount,
+  });
+
+  final int id;
+  final String name;
+  final String quantity;
+  final double amount;
+}
